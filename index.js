c56a3eed53e4321aec2c2469c92d33c3bcc791e0
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2015-12-23 14:23:32 +0100

    Fix results save

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var Sequelize = require('sequelize'); // ORM
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.static('./resources/public'));
 app.use('/admin', auth.basicAuth('admin', 'test'));
 
